cfb25382a3db37ce3d97f8c467143a1abb279c8a
Timothy Makkison <timmakkison@gmail.com>
2021-09-22 22:30:48 +0100

    Edit update return values

--- Triangle Inscriber/MainPage/Model/DelaunayHandler.cs
+++ Triangle Inscriber/MainPage/Model/DelaunayHandler.cs
@@ -11,26 +11,25 @@ public static class DelauneyHandler
         var delauney = new Delaunator(points.ToArray());
 
         var triangles = delauney.GetTriangles();
-        var polyTriangles = triangles.Select(tri => new Poly() 
-        { 
-            Points= tri.Points.ToPointCollection(),
+        return triangles.Select(tri => new Poly()
+        {
+            Points = tri.Points.ToPointCollection(),
             Index = tri.Index
         });
-        return polyTriangles;
     }
-    
+
     public static IEnumerable<Poly> GetVoroni(this IEnumerable<IPoint> points)
     {
         var delauney = new Delaunator(points.ToArray());
 
         var voroni = delauney.GetVoronoiCells();
-        var polyVoroni = voroni.Select(vor => new Poly() 
+        return voroni.Select(vor => new Poly()
         {
             Points = vor.Points.ToPointCollection(),
             Index = vor.Index
         });
-        return polyVoroni;
     }
+
     private static PointCollection ToPointCollection(this IEnumerable<IPoint> points)
     {
         return new PointCollection(points.Select(p => new System.Windows.Point(p.X, p.Y)));
